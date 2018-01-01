5de4f19fd5635f3e241b7e8fee0527f36e292ea6
gsbhz <gsbhzh@gmail.com>
2018-01-01 19:44:03 +0800

    修改build-4.0.bat

--- Build-4.0.bat
+++ Build-4.0.bat
@@ -1,5 +1,5 @@
 @echo off
 
-MSBuild /t:rebuild /p:TargetFrameworkVersion=v4.0;Configuration=Release TiaoYiTiao.csproj
+MSBuild /t:rebuild /p:TargetFrameworkVersion=v4.0;Configuration=Release;OutputPath=bin\release4.0 TiaoYiTiao.csproj
 
 pause
